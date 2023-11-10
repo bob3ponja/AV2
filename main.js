a4f20e7446688bfd09fa2a177ef8c37f84d02398
plants vs zombies <106076221+bob3ponja@users.noreply.github.com>
2023-11-10 10:37:59 -0300

    a

--- main.js
+++ main.js
@@ -2,4 +2,15 @@ function_toca_som(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 
-const listadeteclas = 
+const listadeteclas = document.querySelectorAll(".tecla");
+
+led contador = 0;
+while(contador < listadeteclas.length){
+    const efeito = listadeteclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listadeteclas[contador].onclick = function(){
+        tocasom (idAudio)
+    }
+    contador = contador + 1;
+    console .log(contador);
+}
